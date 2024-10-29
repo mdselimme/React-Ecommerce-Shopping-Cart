7078471d5f6cc063e6d4f98ae7cce1768da10de0
mdselimme <mdselimdev@gmail.com>
2024-10-29 22:06:03 +0600

    add_data

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1 className="text-3xl font-bold bg-green-600 text-white underline">Hello World</h1>
+        
+    </>
+  )
+}
+
+export default App
